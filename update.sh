556e6575824fc862c0a5b4d36389d9d275acfa4e
Nick Young <nick.young@auckland.ac.nz>
2024-10-15 14:41:13 +1300

    remove GHA, remove cache in update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git pull
+rm cache.sqlite
 jupyter nbconvert --to notebook --execute --inplace fetch.ipynb
-git commit -am "auto update" --author="woolies-bot"
+git commit -am "auto update" --author="woolies-bot <ubuntu@api-proxy.auckland-cer.cloud.edu.au>"
 git push
